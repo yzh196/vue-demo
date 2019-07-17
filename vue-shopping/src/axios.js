d36c2d615a75b758879f943ee626ee4d56c75a0b
cuixi <378513892@qq.com>
2019-07-18 00:38:17 +0800

    vue-shopping

---
+++ vue-shopping/src/axios.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import axios from 'axios'
+import vueAxios from 'vue-axios'
+
+Vue.use(vueAxios,axios)
---
+++ vue-shopping/vue.config.js
@@ -0,0 +1,10 @@
+// proxyTable: {
+//   '/api': {
+//     target:'http://www.gov.cn:80',
+//       // secure: false, // 如果是https接口，需要配置这个参数
+//       changeOrigin:true,
+//       pathRewrite:{
+//       '^/api': ''
+//     }
+//   }
+// }
