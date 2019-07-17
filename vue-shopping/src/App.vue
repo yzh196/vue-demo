19fcb33b87bd8c5f0e8f8a760b115e33cda7c65a
cuixi <378513892@qq.com>
2019-07-18 00:42:21 +0800

    vue-shopping

---
+++ vue-shopping/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+      <ul>
+        <li v-for="info in productList">
+          {{info.title}}
+        </li>
+      </ul>
+  </div>
+</template>
+
+
+<script>
+  export default{
+    data: function () {
+      return {
+        totalMonery: {
+          type: Number,
+          require: true
+        },
+        productList: {
+          type: Array,
+          require: true
+        }
+      }
+    },
+    mounted: function () {
+      this.careView()
+    },
+    methods: {
+      careView: function () {
+        this.axios.get('data.json').then(
+          (response) => {
+              console.log(response.data)
+            this.productList = response.data
+          })
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
